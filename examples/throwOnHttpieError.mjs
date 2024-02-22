90abf847bf038d6e87c0d28df3871f1c66e6cd68
SofianD <doual.sofian@hotmail.com>
2024-02-22 17:14:37 +0100

    update example of throwOnHttpError

--- examples/throwOnHttpieError.mjs
+++ examples/throwOnHttpieError.mjs
@@ -2,18 +2,20 @@ import * as httpie from "../dist/index.js";
 // import * as httpie from "@myunisoft/httpie";
 
 // Should not throw
-async() => {
-  const { statusCode } = await httpie.request("GET", "127.0.0.1", { throwOnHttpError: false });
+{
+  const { statusCode } = await httpie.request("GET", "127.0.0.1", {
+    throwOnHttpError: false
+  });
+
   console.log(statusCode) // 500
 }
 
 // Should throw
-async() => {
-  let statusCode;
-  try {
-    (statusCode = await httpie.request("GET", "127.0.0.1", { throwOnHttpError: true }));
-  } catch (error) {
-    console.log(statusCode) // undefined
-    console.log(error.statusCode) // 500
-  }
+try {
+  await httpie.request("GET", "127.0.0.1", {
+    throwOnHttpError: true
+  });
+}
+catch (error) {
+  console.log(error.statusCode) // 500
 }
